e555a0043f75420f2fff220c8a865a6271baa7a5
chosson <chosson@users.noreply.github.com>
2023-10-22 21:39:03 -0400

    Refactored given code and added some features.

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75],
-                        "F": [0, 70]}
-
-# TODO: Importez vos modules ici
-from recettes import add_recipes, print_recipe
+import os
 from os import path
 import pickle
+import json
 
-
-# TODO: Définissez vos fonction ici
+from recettes import add_recipes, delete_recipe, print_recipe, run_interactive
 
 
 def exercice1(file_path1, file_path2):
@@ -27,63 +23,51 @@ def exercice1(file_path1, file_path2):
 
     print("The files are identical")
 
-
 def exercice2(file_path1, file_path2):
     with open(file_path1, encoding="utf-8") as f1, open(file_path2, "w", encoding="utf-8") as f2:
         f2.write(f1.read().replace(" ", "   "))
 
-
-def exercice3(note_path, result_file_path):
+def exercice3(note_path, thresholds_file_path, result_file_path):
     with open(note_path, encoding="utf-8") as note_file:
         notes_percent = note_file.readlines()  # To get rid of \n: note_file.read().splitlines()
 
+    with open(thresholds_file_path, "r") as thresholds_file:
+        letter_grade_thresholds = json.load(thresholds_file)
+
     with open(result_file_path, "w", encoding="utf-8") as result_file:
         for note in notes_percent:
-            for key, value in PERCENTAGE_TO_LETTER.items():
+            for key, value in letter_grade_thresholds.items():
                 if value[0] <= int(note) < value[1]:
                     result_file.write(note.strip() + " " + key + "\n")
                     break
 
-
-def delete_recipe(dictionary):
-    name = input("Entrez le nom de la recette que vous voulez supprimer.\n")
-
-    if name in dictionary:
-        del dictionary[name]
-        print("La recette est supprimée!")
-    else:
-        print("Cette recette n'existe pas!")
-
-    return dictionary
-
-
 def exercice4(recipes_path):
+    file_extension = path.splitext(recipes_path)[1]
+    
+    recipes = {}
     if path.exists(recipes_path):
-        with open(recipes_path, 'rb') as f:
-            recipes = pickle.load(f)
-    else:
-        recipes = dict()
-
-    while True:
-        choice = input(
-            "Choisissez une option: \n a) Ajouter une recette \n b) Modifier une recette \n c) Supprimer une recette \n d) Afficher une recette \n e) Quitter le programme\n").strip()
-
-        if choice == "a":
-            recipes = add_recipes(recipes)
-        elif choice == "b":
-            recipes = add_recipes(recipes)
-        elif choice == "c":
-            recipes = delete_recipe(recipes)
-        elif choice == "d":
-            print_recipe(recipes)
-        elif choice == "e":
-            break
+        if file_extension == ".p":
+            with open(recipes_path, 'rb') as f:
+                # Exemple d'utilisation avec Pickle
+                recipes = pickle.load(f)
+        elif file_extension == ".json":
+            with open(recipes_path, 'r') as f:
+                # Ou en JSON si on veut garder la lecture en texte
+                recipes = json.load(f)
         else:
-            print("L'option choisi n'est pas valide!")
+            print(f"Format de fichier {file_extension} inconnu.")
+            return
 
-    with open(recipes_path, 'wb') as f:
-        pickle.dump(recipes, f)
+    recipes = run_interactive()
 
+    if file_extension == ".p":
+        with open(recipes_path, 'wb') as f:
+            # Exemple d'utilisation avec Pickle
+            pickle.dump(recipes, f)
+    elif file_extension == ".json":
+        with open(recipes_path, 'w') as f:
+            # Ou en JSON si on veut garder la lecture en texte
+            json.dump(recipes, f, indent=2)
 
 def exercice5(file_path):
     with open(file_path, encoding="utf-8") as f:
@@ -93,7 +77,6 @@ def exercice5(file_path):
 
     return sorted(number)
 
-
 def exercice6(file_path1, file_path2):
     with open(file_path1, encoding="utf-8") as f1, open(file_path2, "w", encoding="utf-8") as f2:
         for index, line in enumerate(f1):
@@ -102,12 +85,13 @@ def exercice6(file_path1, file_path2):
 
 
 if __name__ == '__main__':
-    # TODO: Appelez vos fonctions ici
-    exercice1("./exemple.txt", "./exemple2.txt")
-    exercice2("./exemple.txt", "./exemple_copy.txt")
-    exercice3("./notes.txt", "./notes_letter.txt")
-    exercice4("./recettes.p")
-    print(exercice5("./exemple.txt"))
-    exercice6("./notes.txt", "./notes_skip.txt")
+    if not os.path.exists("output"):
+        os.mkdir("output")
 
-    pass
+    exercice1("data/exemple.txt", "data/exemple2.txt")
+    exercice2("data/exemple.txt", "output/exemple_copy.txt")
+    exercice3("data/notes.txt", "data/seuils.json", "output/notes_letter.txt")
+    exercice4("data/recettes.json")
+    print(exercice5("data/exemple.txt"))
+    exercice6("data/notes.txt", "output/notes_skip.txt")
+
--- recettes.py
+++ recettes.py
@@ -1,14 +1,23 @@
 def add_recipes(dictionnaire):
-    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
     name = input("Entrez le nom de votre recette?\n")
-    ingrediants = input("Entrez la liste d'ingrédiants de la recette, svp séparer les ingrédiants par une ,\n").split(",")
-    dictionnaire[name] = ingrediants
+    ingredients = input("Entrez la liste d'ingrédients de la recette, svp séparer les ingrédients par une ,\n").split(",")
+    ingredients = [ing.strip() for ing in ingredients]
+    dictionnaire[name] = ingredients
 
     return dictionnaire
 
+def delete_recipe(dictionary):
+    name = input("Entrez le nom de la recette que vous voulez supprimer.\n")
 
-def print_recipe(ingredients: dict) -> None:
-    # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
+    if name in dictionary:
+        del dictionary[name]
+        print("La recette est supprimée!")
+    else:
+        print("Cette recette n'existe pas!")
+
+    return dictionary
+
+def print_recipe(ingredients):
     name = input("Entrez le nom d'une recette?\n")
 
     if name in ingredients:
@@ -16,3 +25,23 @@ def print_recipe(ingredients: dict) -> None:
     else:
         print("La recette demandée n'existe pas!")
         print(f"Les recettes existantes sont: {list(ingredients.keys())}")
+
+def run_interactive(recipes):
+    while True:
+        choice = input(
+            "Choisissez une option: \n a) Ajouter une recette \n b) Modifier une recette \n c) Supprimer une recette \n d) Afficher une recette \n e) Quitter le programme\n").strip().lower()
+
+        if choice == "a":
+            recipes = add_recipes(recipes)
+        elif choice == "b":
+            recipes = add_recipes(recipes)
+        elif choice == "c":
+            recipes = delete_recipe(recipes)
+        elif choice == "d":
+            print_recipe(recipes)
+        elif choice == "e":
+            break
+        else:
+            print("L'option choisi n'est pas valide!")
+
+    return recipes
